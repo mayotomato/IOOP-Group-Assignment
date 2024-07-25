b6ac7548b8a567f286c151f40ce8df2f6e5cb55b
zakwaan <TP078882@mail.apu.edu.my>
2024-07-25 22:24:29 +0800

    label

--- IOOP Group Assignment/LoginPage.Designer.cs
+++ IOOP Group Assignment/LoginPage.Designer.cs
@@ -41,6 +41,7 @@
             this.NewUserLinkLabel = new System.Windows.Forms.LinkLabel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.LoginDetails.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -193,6 +194,15 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 653);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "label1";
+            // 
             // LoginPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
@@ -201,6 +211,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1768, 1079);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.NewUserLinkLabel);
@@ -234,6 +245,7 @@
         private System.Windows.Forms.Button PasswordVisibilityButton;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
