6fa937094baf6c7f65988c2e9ab8922bce4b8336
ABDULRAHMAN KHALED BAJAMAN <bajamanab@icloud.com>
2024-07-30 13:39:04 +0800

    hu ui

---
+++ IOOP Group Assignment/Receptionist.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IOOP_Group_Assignment
+{
+    public partial class Receptionist : Form
+    {
+        public Receptionist()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void check_out_panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void searchres_panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
