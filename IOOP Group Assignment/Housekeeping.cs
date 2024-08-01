3427544c8c308c715601e8a4e449d336a32357d0
zakwaan <TP078882@mail.apu.edu.my>
2024-08-01 14:39:43 +0800

    class introduced

---
+++ IOOP Group Assignment/Housekeeping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOOP_Group_Assignment
+{
+    internal class Housekeeping
+    {
+        private void Housekeeping() { }
+    }
+}
