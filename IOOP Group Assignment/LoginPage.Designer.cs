f2910ae4b4aa24bc7bc43bd914593efb035a0f86
zakwaan <TP078882@mail.apu.edu.my>
2024-08-14 13:38:22 +0800

    heh

--- IOOP Group Assignment/LoginPage.Designer.cs
+++ IOOP Group Assignment/LoginPage.Designer.cs
@@ -40,6 +40,8 @@
             this.LoginPagePrompt = new System.Windows.Forms.Label();
             this.NewUserLinkLabel = new System.Windows.Forms.LinkLabel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.LoginDetails.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -183,6 +185,23 @@
             this.linkLabel1.Text = "Forgot Password?";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(753, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 67);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 464);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(823, 20);
+            this.textBox1.TabIndex = 18;
+            // 
             // LoginPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -191,6 +210,8 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.NewUserLinkLabel);
             this.Controls.Add(this.LoginPagePrompt);
@@ -224,6 +245,8 @@
         private System.Windows.Forms.LinkLabel NewUserLinkLabel;
         private System.Windows.Forms.Button PasswordVisibilityButton;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
